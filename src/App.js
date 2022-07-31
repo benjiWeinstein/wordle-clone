6b48500232836a5e0a071ce1181e9e9ea0c5a528
Benji Weinstein <benjiweinstein@Benjis-MacBook-Pro.local>
2022-07-31 14:46:56 +0300

    done

--- src/App.js
+++ src/App.js
@@ -8,75 +8,84 @@ function App() {
   const [guesses, setGuesses] = useState(Array(6).fill(null));
   const [currentGuess, setCurrentGuess] = useState("");
   const [isOver, setIsOver] = useState(false);
+  const [solution, setSolution] = useState("")
+
+  useEffect(() => {
+
+    fetch('https://emmanyel368.api.stdlib.com/wordle-api@1.0.1/wordle/')
+      .then(response => response.json())
+      .then(response => {
+        setSolution(response.word[0].toUpperCase())
+        console.log(response.word[0])
+      })
+      .catch(err => console.error(err));
+  }, []);
 
   useEffect(() => {
     window.addEventListener("keydown", handleType);
 
     function handleType(event) {
       if (isOver) {
-        return
+        return;
       }
 
-
       const letter = event.key;
-      const code = event.keyCode
+      const code = event.keyCode;
 
-      // check that keypressed is only a letter or backspace or enter 
-      if ((code < 65 || code > 90) && letter !== "Enter" && letter !== "Backspace" ) return
-
+      // check that keypressed is only a letter or backspace or enter
+      if (
+        (code < 65 || code > 90) &&
+        letter !== "Enter" &&
+        letter !== "Backspace"
+      )
+        return;
 
       setCurrentGuess((oldGuess) => {
-
         if (letter === "Enter") {
-          console.log(oldGuess, SOLUTION, oldGuess===SOLUTION)
           // click enter with full word entered
           if (oldGuess.length === WORD_LENGTH) {
-            if (oldGuess.toUpperCase() === SOLUTION) {
-              setIsOver(true)
+            if (oldGuess.toUpperCase() === solution) {
+              setIsOver(true);
             }
             const currentIndex = guesses.findIndex((g) => g === null);
             const newGuesses = [...guesses];
             newGuesses[currentIndex] = oldGuess;
             setGuesses(newGuesses);
             return oldGuess.length === WORD_LENGTH ? "" : oldGuess + letter;
-          }
-          else 
-            return oldGuess;
+          } else return oldGuess;
         }
 
         if (letter === "Backspace") return oldGuess.slice(0, -1);
-        
 
         return oldGuess.length === WORD_LENGTH ? oldGuess : oldGuess + letter;
       });
     }
 
     return () => window.removeEventListener("keydown", handleType);
-  }, [guesses, isOver]);
-
-
+  }, [guesses, isOver, solution]);
 
   return (
-    <div className="board">
-    <p>Benji's Wordle</p>
-      {guesses.map((guess, ind) => {
-        const currentIndex = guesses.findIndex((g) => g === null);
-        return (
-          <Line
-            key={ind}
-            index={ind}
-            solution={SOLUTION}
-            guess={ind === currentIndex ? currentGuess : guess ?? ""}
-            currentIndex={currentIndex}
-          />
-        );
-      })}
+    <div className="App">
+      <div className="board">
+        <div className="title">Benji's Wordle</div>
+        {guesses.map((guess, ind) => {
+          const currentIndex = guesses.findIndex((g) => g === null);
+          return (
+            <Line
+              key={ind}
+              index={ind}
+              solution={solution}
+              guess={ind === currentIndex ? currentGuess : guess ?? ""}
+              currentIndex={currentIndex}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 function Line({ guess, solution, currentIndex, index }) {
-
   const getColor = (letter, ind, solution) => {
     if (solution.charAt(ind) === letter) {
       return " correct";
@@ -86,7 +95,6 @@ function Line({ guess, solution, currentIndex, index }) {
       return " wrong";
     }
   };
-
 
   return (
     <div className="line">
